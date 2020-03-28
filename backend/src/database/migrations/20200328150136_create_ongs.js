eb2ecefb373b68b9bccbc1d9a72f9cabb54effd3
Mauro Sérgio <maurosergiostm@gmail.com>
2020-03-28 17:21:50 -0300

    Desenvolvimento do Back-end da aplicaçao e organizando o Front-end

---
+++ backend/src/database/migrations/20200328150136_create_ongs.js
@@ -0,0 +1,16 @@
+//O que deve ser feito
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', function (table) {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+//Desfazer os problemas
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
